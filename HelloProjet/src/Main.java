0c963005dc159bc63c10512153a771dd685cc186
trovy <643224933@qq.com>
2020-10-20 15:27:52 +0800

    冲突2

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,9 +5,9 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("我是asdafgsdg46541老大!");
-        System.out.println("我是asdafgsdg老大!");
-        System.out.println("我是zx!");
+        System.out.println("我是asdafgsdioghPvbIbipagg46541老大!");
+        System.out.println("我是asdafgsgkdkararejogidg老大!");
+        System.out.println("我是zahgadfhx!");
         System.out.println("我是zx!");
     }
 }
