7b5d5091e08a9235bb5c42c06dee2bd4d3ade7ef
trovy <643224933@qq.com>
2020-10-20 15:24:03 +0800

    出发冲突2

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,8 +5,9 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("我是asdafgsdg46541老大!");
         System.out.println("我是asdafgsdg老大!");
-        System.out.println("我是asdafgsdg老大!");
-        System.out.println("我是trovy!");
+        System.out.println("我是zx!");
+        System.out.println("我是zx!");
     }
 }
